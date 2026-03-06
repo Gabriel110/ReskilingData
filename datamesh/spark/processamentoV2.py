dd8240fc60dcde9dbab1bda1301a4abf40ec958c
Gabriel110 <gabrielcorreiadev@gmail.com>
2026-03-06 08:30:09 -0300

    ok

--- datamesh/spark/processamentoV2.py
+++ datamesh/spark/processamentoV2.py
@@ -2,30 +2,28 @@ import sys
 import logging
 import traceback
 from pyspark.sql import functions as F 
-from awsglue.context import SparkContext
+from awsglue.context import SparkContext, GlueContext
 from datetime import datetime, timedelta
 from awsglue.utils import getResolvedOptions
 
 
-def load_and_register_table(glue_context, table_config, partition_predicate):
+def load_and_register_table(glue_context, table_config, year=None, month=None, day=None):
     table_name = table_config["table_name"]
     columns = table_config["columns"]
     database = table_config["database"]
 
-    yar_str = str(partition_predicate[0]).zfill(2)
-
     try:
-        dynamic_frame glue_context.create_dynamic_frame.from_catalog(
+        dynamic_frame = glue_context.create_dynamic_frame.from_catalog(
             database=database,
             table_name=table_name,
-            push_down_predicate=partition_predicate,
+            push_down_predicate="xxx",
             additional_options={"columns":columns},
             transformation_ctx=f"{table_name}_dynamic_frame"
         )
         df = dynamic_frame.toDF().toDF(*[col.lower() for col in dynamic_frame.toDF().columns])
         df.createOrReplaceTempView(table_name)
     except Exception as e:
-        pass
+        raise
     
 def get_partition_date(args):
     year, month, day = args.get('year'), args.get('month'), args.get('day')
@@ -33,7 +31,7 @@ def get_partition_date(args):
         partition_date = datetime.now() - timedelta(days=1)
     else:
         partition_date = datetime(year=int(year), month=int(month), day=int(day))
-    return (partition_date.strftime('%Y'), partition_date.strftime('%m').lstrip(0), partition_date.strftime('%d').lstrip(0))
+    return partition_date.strftime('%Y'), partition_date.strftime('%m').lstrip(0), partition_date.strftime('%d').lstrip(0)
 
 def main():
     sc = SparkContext.getOrCreate()
@@ -50,12 +48,12 @@ def main():
             }
         }
 
-        partition_date = get_partition_date(args)
-        for table_key, table_config int TABLE_CONFIG.items():
-            load_and_register_table(glueContext, table_config, partition_date)
+        year, month, day = get_partition_date(args)
+        for table_key, table_config in TABLE_CONFIG.items():
+            load_and_register_table(glueContext, table_config, year, month, day)
         resultado_query = spark.sql("SELECT * FROM xxx")
         resultado_query = resultado_query.dropDuplicates()
         resultado_query.show()
 
     except Exception as e:
-        pass
+        raise
